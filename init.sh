88d06a66a4f717e40f836205a477a6193f753692
Karolis Šarapnickis <pastas.k@gmail.com>
2022-01-11 01:08:21 +0200

    add terminal helpers

--- init.sh
+++ init.sh
@@ -3,7 +3,3 @@
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 brew update
 brew install ansible
-
-chmod u+x setup.sh
-chmod u+x clean.sh
-chmod u+x restow.sh

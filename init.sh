0fbc85ec58c704344c95f3c0476562bcba28c7e6
Karolis Šarapnickis <hi@karolis.sh>
2022-05-16 15:23:28 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> $HOME/.profile
-eval "$(/opt/homebrew/bin/brew shellenv)”
 brew update
 brew install ansible

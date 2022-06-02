dd2769b2cecfdfad2e0098cf0c9a30b892a64bab
Karolis Šarapnickis <hi@karolis.sh>
2022-06-02 13:47:49 +0300

    update gitconfig setup

--- restow.sh
+++ restow.sh
@@ -4,4 +4,6 @@ if [ $(find ~ -maxdepth 1 -iname ".zshrc" -type f) ]; then
   mv -v ~/.zshrc ~/.zshrc_back
 fi
 
+git config --global include.path "~/.gitconfig.personal"
+
 stow --restow --target=$HOME dotfiles --verbose "$@"

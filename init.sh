9526662b3431ffc094a82a583dba7e09843c770c
Karolis Šarapnickis <hi@karolis.sh>
2022-06-02 13:53:34 +0300

    tweak init script

--- init.sh
+++ init.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+which brew || /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 brew update
 brew install ansible
